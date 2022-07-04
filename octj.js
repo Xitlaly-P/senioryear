1cc049b524d0fb8c506d7d478fd8af56a0d589c0
Xitlaly-P <jada23dem@gmail.com>
2022-07-04 15:10:48 -0400

    october save

--- octj.js
+++ octj.js
@@ -48,3 +48,20 @@ $(document).ready(function () {
 	  }
 	});
   });
+
+var image_tracker = 'one';
+ 
+ function change(){
+    var image = document.getElementById('circle');
+    if(image_tracker=='one'){
+        image.src='month/oct/octe2.jpg';
+        image_tracker='two';
+    }
+    else if(image_tracker=='two'){
+        image.src='month/oct/octe3.jpg';
+        image_tracker='three';
+    } else{
+        image.src='month/oct/octe1.jpg';
+        image_tracker='one';
+    }
+ }
